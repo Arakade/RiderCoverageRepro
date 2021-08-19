a262bb973afcf154ae4eaff905f6b6eedf50ffa9
Rupert Key <Rupert.Key@GMail.com>
2021-08-19 22:16:42 +0100

    Simple project that coverage doesn't seem to work on.

---
+++ RiderCovLib1/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace RiderCovLib1 {
+	public class Class1 {
+
+		public string sayHi() {
+			return "Hi!"; // covered
+		}
+
+		public string greet(string whom) {
+			return $"Hi, {whom}!"; // uncovered
+		}
+
+	}
+}
---
+++ RiderTestCov1Project1/Tests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using RiderCovLib1;
+
+namespace RiderTestCov1Project1 {
+	[TestFixture]
+	public class Tests {
+
+		[Test]
+		public void greetWorks() {
+			Assert.That(new Class1(), Is.Not.Null);
+			Assert.That(new Class1().sayHi(), Is.Not.Null);
+			Assert.That(new Class1().sayHi(), Is.EqualTo("Hi!"));
+		}
+
+	}
+}
